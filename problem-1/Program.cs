5f9982d7b10efc1f6961f7cd016dc8f5379f398d
amjed <amjed1@live.com>
2018-05-13 17:16:46 +0100

    use arithmetic sum

--- problem-1/Program.cs
+++ problem-1/Program.cs
@@ -15,30 +15,81 @@ namespace problem_1
         }
     }
 
-    public class Solution
+    public interface ISolution
     {
-        public int FindSumForRange(int max)
+        int FindSumForRange(int start, int end, int diff);
+    }
+
+    public class Solution: ISolution
+    {
+        public int FindSumForRange(int start, int end, int diff)
         {
             int sum = 0;
 
-            for(int i =0 ;i< max; i++)
+            for(int i =start ;i<= end; i++)
             {
-                if(i.IsMultipleOf(3) || i.IsMultipleOf(5)) 
+                if(i.IsMultipleOf(diff)) 
                     sum += i;
             }
             return sum;
         }
     }
 
+    public class Solution2:ISolution
+    {
+        private int FindTerms(int a, int an, int d)
+        {
+            return (an - a + d) / d;
+        }
+
+        private int SumTerms(int n, int a, int d)
+        {
+            return (n*(2*a + (n-1)*d))/2;
+        }
+
+        public int FindSumForRange(int start, int end, int diff)
+        {
+            var n = FindTerms(start, end, diff);
+            var sum = SumTerms(n, start , diff);
+            return sum;
+        }
+    }
+
+
     class Program
     {
+        static void UseSolution()
+        {
+            var start = DateTime.Now;
+            int range = 1000-1;
+            var sol = new Solution();
+            var sumOfMultiplesOfThree = sol.FindSumForRange(3, range, 3);
+            var sumOfMultiplesOfFive = sol.FindSumForRange(5, range, 5);
+            var sumOfMultiplesOfFifteen = sol.FindSumForRange(15, range, 15);
+            var time = (DateTime.Now - start).Milliseconds;
+
+            Console.WriteLine($"sum of multiples in range 0<x<{range} is: {sumOfMultiplesOfThree+sumOfMultiplesOfFive - sumOfMultiplesOfFifteen}");
+            Console.WriteLine($"took: {time}ms");
+        }
+
+        static void UseSolution2()
+        {
+            var start = DateTime.Now;
+            int range = 1000-1;
+            ISolution sol = new Solution2();
+            var sum = sol.FindSumForRange(3, range, 3);
+            var sum2 = sol.FindSumForRange(5, range, 5);
+            var sum3 = sol.FindSumForRange(15, range, 15);
+            var time = (DateTime.Now - start).Milliseconds;
+
+            Console.WriteLine($"sum of multiples in range 0<x<{range} is: {sum+sum2 - sum3}");
+            Console.WriteLine($"took: {time}ms");
+        }
+
         static void Main(string[] args)
         {
-            int range = 1000;
-            var sol = new Solution();
-            var sum = sol.FindSumForRange(range);
-
-            Console.WriteLine($"sum of multiples in range 0<x<{range} is: {sum}");
+            UseSolution2();
+            UseSolution();
         }
     }
 }
